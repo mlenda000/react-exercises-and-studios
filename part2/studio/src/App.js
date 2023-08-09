2d5b272eb5db9b9ac6c84d46bbdbf1259f04b2a2
Mark <138724906+mlenda000@users.noreply.github.com>
2023-08-09 12:53:54 -0400

    updated

--- part2/studio/src/App.js
+++ part2/studio/src/App.js
@@ -17,7 +17,7 @@ export default function App() {
       {/* <SaveButton /> */}
       {/* <ClickedButton/> */}
       <Button saveButton={true} />
-       <AuthorInfo />
+      <AuthorInfo />
       <IngredientList />  
     </>
   );
